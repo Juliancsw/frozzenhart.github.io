905d321c4dd5b881572e92b5357249f90d521338
Julian Chan <frozzenhart@hotmail.com>
2016-01-20 19:37:18 +0800

    Revert "new commit"
    This reverts commit 3014682fb22f6d8701e5a70d3758688b4894fe49.

--- scripts/main.js
+++ scripts/main.js
@@ -1,34 +1,34 @@
 alert("Hello!");
 
-/*document.querySelector('html').onclick = function(){
+document.querySelector('html').onclick = function(){
 		alert("Ouch! Stop poking me!");
-}*/
+}
 
 var myImage = document.querySelector('img');
 
 myImage.onclick = function(){
 	var mySrc=myImage.getAttribute('src');
-	alert("Surprise!!!");
-	if(mySrc==='images/clickme.jfif'){
-		myImage.setAttribute('src','images/TPbirthday.jpg');
+	alert("Watch the magic!");
+	if(mySrc==='images/hi.jpg'){
+		myImage.setAttribute('src','images/download.jpg');
 	}else{
-		myImage.setAttribute('src','images/clickme.jfif');
+		myImage.setAttribute('src','images/hi.jpg');
 	}
 }
 
 var myButton=document.querySelector('button');
 var myHeading=document.querySelector('h1');
 function setUserName(){
-	var myName = prompt('Whose birthday is it?');
+	var myName = prompt('Please enter your name.');
 	localStorage.setItem('name', myName);
-	myHeading.innerHTML = 'Happy Birthday '+myName+'!!!';
+	myHeading.innerHTML = 'Welcome to this page, '+myName;
 }
 
 if(!localStorage.getItem('name')){
 	setUserName();
 }else{
 	var storedName = localStorage.getItem('name');
-	myHeading.innerHTML = "Happy Birthday "+storedName+'!!!'; //note the diff from above, website/page.
+	myHeading.innerHTML = "Welcome to this website, "+storedName; //note the diff from above, website/page.
 }
 
 myButton.onclick = function(){
